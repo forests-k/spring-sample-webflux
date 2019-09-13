bd78f0662831fcde8e9663aa4c79a57d6b654716
forest-k <kzuki.mori@gmail.com>
2019-09-13 17:07:48 +0900

    refactor

--- src/main/kotlin/jp/co/musako/domain/repository/FoodstuffRepository.kt
+++
@@ -1,9 +0,0 @@
-package jp.co.musako.domain.repository
-
-import jp.co.musako.domain.entity.Foodstuff
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface FoodstuffRepository : JpaRepository<Foodstuff, Int> {
-}
