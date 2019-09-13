c674557fd15754b14fb94f6cf1bb58990a3e6ad6
forest-k <kzuki.mori@gmail.com>
2019-09-13 17:08:11 +0900

    refactor using Flux

--- src/main/kotlin/jp/co/musako/domain/service/UserService.kt
+++ src/main/kotlin/jp/co/musako/domain/service/UserService.kt
@@ -2,13 +2,13 @@ package jp.co.musako.domain.service
 
 import jp.co.musako.domain.repository.UserRepository
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import reactor.core.publisher.toFlux
 
 @Service
 class UserService(val userRepository: UserRepository) {
 
     fun findById(id: Int) = Mono.justOrEmpty(userRepository.findById(id))
 
-    fun findAll() = userRepository.findAll().toFlux()
+    fun findAll() = Flux.fromIterable(userRepository.findAll())
 }
